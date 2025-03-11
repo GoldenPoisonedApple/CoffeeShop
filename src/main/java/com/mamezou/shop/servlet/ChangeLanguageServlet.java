15fb1dfaf69c1a8d4a368ba2623c02443dae501c
ito <nanisiyou1@gmail.com>
2025-03-12 01:18:42 +0900

    #8 create: langage.jsp
    言語切替機能のプロトタイプ作成 language.jsp

---
+++ src/main/java/com/mamezou/shop/servlet/ChangeLanguageServlet.java
@@ -0,0 +1,41 @@
+package com.mamezou.shop.servlet;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/change-language")
+public class ChangeLanguageServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// リクエスト元URL取得
+		String referer = request.getHeader("Referer");
+		if (referer == null) {
+			referer = "index.html";
+		}
+		// デバッグ用
+		Logger logger = LogManager.getLogger(ChangeLanguageServlet.class.getName());
+		logger.debug("Referer: " + referer);
+
+		// 言語取得
+		String language = request.getParameter("lang");
+
+		// セッションオブジェクトを取得
+		HttpSession session = request.getSession();
+		// セッションスコープに言語設定
+		session.setAttribute("language", language);
+
+		// リクエスト元にリダイレクト
+		response.sendRedirect(referer);
+	}
+	
+}
